c326daf1433394155dd950261ca0b59f5fb550b1
Davi Nascimento de Jesus <davinasc.dejesus@gmail.com>
2024-03-08 21:27:38 -0300

    Aula de blockchain

---
+++ workshop/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="flex border-2 justify-between bg-black">
+        <p className="text-4xl text-bold text-blue-300">Hello, world!</p>
+        <p className=" text-white text-underline">Olá, inteli.</p>
+    </div>
+  );
+}
+
+export default App;
